3feec4a1f666e3c98247197f3fed7e4eb262e97c
hatem-mohamed-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-03-19 17:57:00 +0200

    ending the caesar alrorithm

---
+++ algorithms/PolyalphabeticSubstitution.php
@@ -0,0 +1,104 @@
+<?php
+
+
+namespace Elsheref\Security\Algorithms;
+
+
+class PolyalphabeticSubstitution
+{
+    const SMALL = 'small_only';
+    const CAPITAL = 'with_capital';
+
+    private int $key;
+    private bool $supportSpace = false;
+    private bool $supportCapital = false;
+    public array $domain = [];
+
+    /**
+     * Caesar constructor.
+     * @param int $key
+     * @param bool $capital
+     * @param bool $space
+     */
+    public function __construct(int $key , bool $capital = false , bool $space = false)
+    {
+        $this->key = $key;
+        $this->supportSpace = $space;
+        $this->supportCapital = $capital;
+        $this->generateDomain($capital ? self::CAPITAL : self::SMALL , $space);
+
+    }
+
+    private function generateDomain(string $domain , bool $space = false){
+
+        switch ($domain){
+            case self::SMALL:
+                $this->domain  = array_merge($this->domain , range('a','z'));
+                break;
+            case self::CAPITAL:
+                $capital = range('A','Z');
+                $small = range('a','z');
+                $this->domain = array_merge($capital,$small);
+                break;
+            default:
+                $this->domain  = array_merge($this->domain , range('a','z'));
+        }
+
+        if ($space)
+            array_unshift($this->domain , ' ');
+
+        $this->domain = array_flip($this->domain);
+
+    }
+
+    private function prepareText(string $text){
+        if (empty($text))
+            throw new \Exception('please enter valid text');
+
+        if (!$this->supportSpace){
+            $text = implode('',explode(' ',$text));
+        }
+        if (!$this->supportCapital)
+            $text = strtolower($text);
+
+        return $text;
+    }
+
+    public function encrypt(string $plain){
+        $result = [];
+        try {
+            $plain = $this->prepareText($plain);
+        }catch (\Exception $e) {
+            echo $e->getMessage();
+            exit(0);
+        }
+
+        foreach (str_split($plain) as $index => $char){
+            $cipher = ($this->domain[$char] + $this->key) % count($this->domain);
+            $cipher = array_flip($this->domain)[$cipher];
+            array_push($result,$cipher);
+        }
+        return implode('',$result);
+    }
+
+    public function decrypt(string $cipher){
+
+        $result = [];
+
+        try {
+            $cipher = $this->prepareText($cipher);
+        }catch (\Exception $e) {
+            echo $e->getMessage();
+            exit(0);
+        }
+        foreach (str_split($cipher) as $index => $char){
+            $plain = ($this->domain[$char] - $this->key) % count($this->domain);
+            if ($plain < 0 )
+                $plain = $plain + count($this->domain);
+
+            $plain = array_flip($this->domain)[$plain];
+            array_push($result,$plain);
+        }
+        return implode('',$result);
+    }
+}
---
+++ run.php
@@ -0,0 +1,12 @@
+<?php
+
+// require the composer autoloader file to resolve the namespaces of classes
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+################## try caesar algorithm ########################
+//$caesar = new \Elsheref\Security\Algorithms\Caesar(4 , false , true);
+//echo $caesar->encrypt('hatem mohamed elsheref').PHP_EOL;
+//echo $caesar->decrypt('lexiqdqsleqihdipwlivij').PHP_EOL;
+
+################## try polyalphabetic substitution algorithm ########################
+
